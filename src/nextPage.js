c8dc654b52baefc4723e7d3446c0d18d2ea9a2fb
Duzzyn <eduardonense05@gmail.com>
2025-01-30 13:25:03 -0300

    :fire: Remove function useless

--- src/nextPage.js
+++ src/nextPage.js
@@ -1,14 +1,17 @@
 async function temProximaPagina(page) {
-    const proximo = await page.$("#ctl00_ContentBody_lkbPaginacaoTopProximo")
-    return proximo !== null;
-}
-
-async function proximaPagina(page) {
-    await page.click("#ctl00_ContentBody_lkbPaginacaoTopProximo") ESTÁ RETORNANDO NULL
-    await page.waitForNavigation()
+  const proximo = await page.$("#ctl00_ContentBody_lkbPaginacaoTopProximo");
+  if (!proximo) {
+    return false;
+  } else {
+    const botaoVisivel = await page.evaluate(el => {
+      const style = window.getComputedStyle(el);
+      return style.display !== "none";
+    }, proximo);
+    console.log(botaoVisivel)
+    return botaoVisivel;
+  }
 }
 
 module.exports = {
-    temProximaPagina,
-    proximaPagina
-}
+  temProximaPagina,
+};
